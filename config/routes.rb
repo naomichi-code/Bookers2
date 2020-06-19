f1f8d1fcbf5c479273c124a10c6cff7de1294388
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-19 13:29:43 +0900

    add name login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
   
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
